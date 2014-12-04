d2c5e6fd73b84dbf2af4f7d1eae05b7ff46a4636
Rashad <rjrusse2@illinois.edu>
2014-12-03 22:17:27 -0600

    First Push

---
+++ SixthSense.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {
+
+	var disableIt,
+		hasBorders,
+		port;
+
+
+	function setPort(callback) {
+	    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+	        port = chrome.tabs.connect(tabs[0].id, {name: "knockknock"});
+	        callback(port);
+	    });
+	}
+
+	function rtSetPort(callback) {
+		chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+	        port = chrome.tabs.connect(tabs[0].id, {name: "knockknock"});
+	        callback(port);
+	    });
+	}
+
+
+	function restore_options() {
+
+		chrome.storage.local.get([
+			'disableCSS',
+			'hasBorders'
+
+		], function(items) {
+
+			$('.css-control #disableCSS').prop('checked', items.disableCSS);
+			$('.boarder-control #addBoarders').prop('checked', items.hasBorders);
+		});
+
+	}
+	restore_options();
+
+
+
+	setPort(function(port) {
+
+		if(port) {
+
+			port.onMessage.addListener(function(msg) {
+				if(msg.loadDataReq) {
+
+					chrome.storage.local.get([
+						'disableCSS',
+
+					], function(items) {
+						
+						port.postMessage({stylechange: "disableCSS", disableIt: items.disableCSS});
+
+					});
+
+				}
+
+			});
+			
+		} else {
+			console.log('No Port Was Set!');
+		}
+
+	});
+
+	
+
+	$('.font-size-control #incFontSize').on('click', function(e) {
+
+		setPort(function(port) {
+			
+			if(port) {
+
+				port.postMessage({stylechange: "fontsize", fontsize: "increase"});
+
+			} else {
+				console.log('No Port Was Set!');
+			}
+
+		});
+		
+
+	});
+
+	$('.font-size-control #decFontSize').on('click', function(e) {
+
+		setPort(function(port) {
+			
+			if(port) {
+
+				port.postMessage({stylechange: "fontsize", fontsize: "decrease"});
+
+			} else {
+				console.log('No Port Was Set!');
+			}
+
+		});
+		
+
+	});
+
+	$('.css-control #disableCSS').on('click', function(e) {
+
+		disableIt = e.currentTarget.checked;
+
+		disableIt = disableIt ? true : false;
+
+		setPort(function(port) {
+
+			if(port) {
+
+				port.postMessage({stylechange: "disableCSS", disableIt: disableIt});
+
+				chrome.storage.local.set({disableCSS: disableIt});
+
+			} else {
+				console.log('No Port Was Set!');
+			}
+
+		});
+
+	});
+
+
+	$('.border-control #addBorders').on('click', function(e) {
+
+		hasBorders = e.currentTarget.checked;
+
+		hasBorders = hasBorders ? true : false;
+
+		setPort(function(port) {
+
+			if(port) {
+
+				port.postMessage({stylechange: "addBorders", hasBorders: hasBorders});
+
+				chrome.storage.local.set({hasBorders: hasBorders});
+
+			} else {
+				console.log('No Port Was Set!');
+			}
+
+		});
+
+	});
+
+
+
+});
---
+++ content_script.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+
+
+    chrome.storage.local.get([
+        'disableCSS',
+        'hasBoarders'
+
+    ], function(items) {
+
+        if(items.disableCSS === true) {
+
+            $('link[rel="stylesheet"]').attr('disabled', 'disabled');
+
+        } else if(items.disableCSS === false) {
+
+            $('link[rel="stylesheet"]').removeAttr('disabled');
+        }
+
+    });
+
+
+
+    chrome.extension.onConnect.addListener(function(port) {
+
+      port.onMessage.addListener(function(msg) {
+
+        if (msg.stylechange === "fontsize") {
+            var fontSize = parseInt($('body').css('font-size')),
+                lineHeight = fontSize;
+            
+            if(msg.fontsize === "increase") {
+                fontSize = fontSize + 1;
+                lineHeight = lineHeight + 5;
+            } else {
+
+                fontSize = (fontSize - 1);
+                lineHeight = (fontSize + 5);
+            }
+
+            fontSize += 'px';
+            lineHeight += 'px';
+
+
+            $('*').attr('style', 'font-size: ' + fontSize + ' !important; line-height: ' + lineHeight + ' !important');
+
+        } else if(msg.stylechange === "disableCSS") {
+            
+            if(msg.disableIt === true) {
+
+                $('link[rel="stylesheet"]').attr('disabled', 'disabled');
+
+            } else if(msg.disableIt === false) {
+
+                $('link[rel="stylesheet"]').removeAttr('disabled');
+            }
+            
+        } else if(msg.stylechange === "addBorders") {
+
+            if(msg.hasBorders === true) {
+                console.log('About to Add Borders');
+                $('p, img, video, object, a, h1, h2, h3, h4, h5, h6, li').css('border-style', 'solid');
+                $('p, img, video, object, a, h1, h2, h3, h4, h5, h6, li').css('border-width', '2');
+                $('p, img, video, object, a, h1, h2, h3, h4, h5, h6, li').css('border-color', 'blue');
+
+            } else if(msg.hasBorders === false) {
+                $('p, img, video, object, a, h1, h2, h3, h4, h5, h6, li').css('border-style', 'none');
+            }
+        }
+          
+      });
+
+    });
+
+});
+
